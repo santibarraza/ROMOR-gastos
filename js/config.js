f96ba1304342e9b20dbcb9feb411911f0b8f775c
santibarraza <Santibarraza@gmail.com>
2026-08-17 21:25:48 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
 // (los obtienes en: Supabase Dashboard > Project Settings > API)
 // =============================================================
 window.APP_CONFIG = {
-  SUPABASE_URL: "https://pdzjgzyhcxrrdtypflga.supabase.co/rest/v1/",
+  SUPABASE_URL: "https://pdzjgzyhcxrrdtypflga.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkempnenloY3hycmR0eXBmbGdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwMTI3MTYsImV4cCI6MjEwMjU4ODcxNn0.TnrgFU1mnt-Jx2JUxviRfSmcu2jOnOAcHcsFBWm-f_Q",
 
   // Correo del usuario "compartido" que crearás en Supabase Auth.

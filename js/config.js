7e0306319b9dafe29706efbcc89806ca08e6f436
santibarraza <Santibarraza@gmail.com>
2026-09-08 18:23:54 -0700

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.APP_CONFIG = {
   SUPABASE_URL: "https://pdzjgzyhcxrrdtypflga.supabase.co",
   SUPABASE_ANON_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBkempnenloY3hycmR0eXBmbGdhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODcwMTI3MTYsImV4cCI6MjEwMjU4ODcxNn0.TnrgFU1mnt-Jx2JUxviRfSmcu2jOnOAcHcsFBWm-f_Q",
-  SHARED_LOGIN_EMAIL: "equipo@romor-obra.local",
+  SHARED_LOGIN_EMAIL: "equipo@romor-obra.local",ADMIN_LOGIN_EMAIL: "santiago@romor-obra.local",
   APP_NAME: "ROMOR · Gastos de obra",
 };
